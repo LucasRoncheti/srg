3a6b49bc59aa4ad38acdbaeb5f6b8e0c63e0d2de
Lucas Roncheti <109318375+LucasRoncheti@users.noreply.github.com>
2023-09-06 16:11:22 -0300

    pagina cadastros e mobile menu prontos
    css js e html prontos

---
+++ mobileMenu/js/mobileMenu.js
@@ -0,0 +1,22 @@
+
+
+menuMobile = document.getElementById('mobileMenu')
+
+condicional = 1
+//função que abre o menu mobile 
+
+openMenu = () => {
+   if(condicional == 1){
+    menuMobile.classList.add('openAnimation')
+    menuMobile.classList.remove('closeAnimation')
+    menuMobile.style.display='block'
+    condicional = 0
+   }else{
+    menuMobile.classList.add('closeAnimation')
+    menuMobile.classList.remove('openAnimation')
+    condicional=1
+    setTimeout(function(){
+        menuMobile.style.display='none'
+    },700)
+   }
+}
