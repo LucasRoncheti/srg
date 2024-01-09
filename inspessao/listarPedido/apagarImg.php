d13513697842005cf9ec06d59c24b791ad3c6cff
Lucas Roncheti <109318375+LucasRoncheti@users.noreply.github.com>
2024-01-09 12:45:17 -0300

    update  apagar imagem hd

--- inspessao/listarPedido/apagarImg.php
+++ inspessao/listarPedido/apagarImg.php
@@ -1,12 +1,12 @@
 <?php
 include '../../generalPhp/conection.php';
-if(!isset($_SESSION)) {
+if (!isset($_SESSION)) {
     session_start();
 }
 
-if(!isset($_SESSION['id'])) {
-    die( header("Location: ../../index.php"));
-   
+if (!isset($_SESSION['id'])) {
+    die(header("Location: ../../index.php"));
+
 }
 
 
@@ -19,13 +19,23 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $stmt = "DELETE FROM imagens WHERE id = ?";
     $stmt = $conn->prepare($stmt);
 
+
+
     if ($stmt) {
         $stmt->bind_param("i", $id_img);
         if ($stmt->execute()) {
-            echo json_encode(array(
-                'success' => true,
-                'message' => 'Imagem apagada com sucesso.'
-            ));
+            
+            //deleta a imagem em hd 
+            $sqlImg = "DELETE FROM imagensalta WHERE id = '$id_img'";
+            $resultsqlimg = $conn->query($sqlImg);
+
+
+            echo json_encode(
+                array(
+                    'success' => true,
+                    'message' => 'Imagem apagada com sucesso.'
+                )
+            );
 
 
             //apaga a imagem thumb nail  na pasta
@@ -33,32 +43,34 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
             if (file_exists($caminhoDoArquivo)) {
                 if (unlink($caminhoDoArquivo)) {
-                   
+
                 }
             }
-             //apaga a imagem high na pasta
-             $caminhoDoArquivo1 = $pathHD;
+            //apaga a imagem high na pasta
+            $caminhoDoArquivo1 = $pathHD;
 
-             if (file_exists($caminhoDoArquivo1)) {
-                 if (unlink($caminhoDoArquivo1)) {
-                   
-                 }
-             }
+            if (file_exists($caminhoDoArquivo1)) {
+                if (unlink($caminhoDoArquivo1)) {
+
+                }
+            }
 
         } else {
-            echo json_encode(array(
-                'success' => false,
-                'message' => 'Erro ao apagar a imagem no banco de dados.'
-            ));
+            echo json_encode(
+                array(
+                    'success' => false,
+                    'message' => 'Erro ao apagar a imagem no banco de dados.'
+                )
+            );
         }
         $stmt->close();
     } else {
-        echo json_encode(array(
-            'success' => false,
-            'message' => 'Erro na preparação da declaração SQL.'
-        ));
+        echo json_encode(
+            array(
+                'success' => false,
+                'message' => 'Erro na preparação da declaração SQL.'
+            )
+        );
     }
 }
 ?>
-
-
