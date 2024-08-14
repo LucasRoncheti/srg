a2980ab2cd376c9d16e9a3a8db921bbd9b2766be
Lucas Roncheti <109318375+LucasRoncheti@users.noreply.github.com>
2024-08-14 16:36:38 -0300

    sagfsddsfhg

--- packingList/listar.js
+++ packingList/listar.js
@@ -5,25 +5,23 @@ var qnt_result_pg = 10; //quantidade de registro por página
 var pagina = 1; //página inicial
 
 
-//  está listando direto da página php  principal  
-// gambiarra da desgraça 
-// $(document).ready(function () {
-//     listar(pagina, qnt_result_pg);
+$(document).ready(function () {
+    listar(pagina, qnt_result_pg);
    
-// });
+});
 
-// function listar(pagina, qnt_result_pg) {
-//     //varia´veis que serão enviadas pelo metodo post para o php 
-//     var dados = {
-//         pagina: pagina,
-//         qnt_result_pg: qnt_result_pg
-//     }
-//     $.post('listar.php', dados , function(retorna){
-//         //seletor id no html
-//         $("#containerList").html(retorna);
-//     });
+function listar(pagina, qnt_result_pg) {
+    //varia´veis que serão enviadas pelo metodo post para o php 
+    var dados = {
+        pagina: pagina,
+        qnt_result_pg: qnt_result_pg
+    }
+    $.post('listar.php', dados , function(retorna){
+        //seletor id no html
+        $("#containerList").html(retorna);
+    });
 
-//     }
+    }
 
 
 
